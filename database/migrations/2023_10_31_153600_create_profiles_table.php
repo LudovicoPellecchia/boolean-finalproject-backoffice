ed22cd420616006d186fbe18ffe2230a3689debd
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-10-31 17:14:20 +0100

    aggiunta migrations

---
+++ database/migrations/2023_10_31_153600_create_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->text('photo')->nullable();
+            $table->string('phone', 20);
+            $table->string('location',50);
+            $table->text('description')->nullable();
+            $table->text('skills');
+            $table->text('curriculum')->nullable();
+            $table->boolean('visible')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
