d07932270a64743d39e5f8ff13fe9b6ca9f289bd
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-11-03 17:54:10 +0100

    aggiunta controller/rotte api

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function index(){
+        $users = User::with('specializations')->paginate();
+
+        return response()->json($users);
+    }
+
+    public function show($id)
+    {
+        $profiles = Profile::where('id', $id)->with('user')->paginate();
+        return response()->json($profiles);
+    }
+
 }
