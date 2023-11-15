126f05a8964e97f94b24d437b241b6d780ec7bae
dariofusco <fdario@hotmail.it>
2023-11-15 11:29:14 +0100

    fix profile card radius

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -181,7 +181,7 @@
         .card {
             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
             background-color: rgba(51, 51, 51, 0.6);
-            border-top-right-radius: 40px;
+            border-radius: 10px;
             color: #b0b1b2;
         }
     </style>
